625ac6ea3bd302fb4ac7fbddd90e36440105b4cc
Yiming Lu <luyiming929@gmail.com>
2020-02-21 15:52:13 +0800

    Site updated: 2020/02/21 15:52

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'sr-precache-0c7f02bc8945a4fb701ca4f667f7053bbc57793e';
+const cacheName = 'sr-precache-92bf580ea3e78b5bf9d9d2ef85671d664faf835b';
 
 const PrecacheList = [
   "/",
@@ -13,11 +13,11 @@ const PrecacheList = [
   "/manifest.json",
   "/favicon.ico",
   "billing/index.html",
-  "billing/static/css/2.dd37c33d.chunk.css",
-  "billing/static/css/main.65de1f4a.chunk.css",
-  "billing/static/js/2.a22d1113.chunk.js",
-  "billing/static/js/2.a22d1113.chunk.js.LICENSE",
-  "billing/static/js/main.c0a23947.chunk.js",
+  "billing/static/css/2.508ce91e.chunk.css",
+  "billing/static/css/main.5e93de2c.chunk.css",
+  "billing/static/js/2.32565dbf.chunk.js",
+  "billing/static/js/2.32565dbf.chunk.js.LICENSE",
+  "billing/static/js/main.83a40844.chunk.js",
   "billing/static/js/runtime-main.072db1e7.js",
   "post/2009/luck.html",
   "post/2009/yuanyuan.html",
